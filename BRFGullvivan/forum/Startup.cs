4b314737b0245e8553eff0220a37e99248881d8c
SebastianForsell <sebastian@brantevik.net>
2016-09-14 11:32:22 +0200

    Start

---
+++ BRFGullvivan/forum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(forum.Startup))]
+namespace forum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
